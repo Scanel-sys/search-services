904f5c602073e4564693ebf9774a229f1d26d92b
bilan.nikita <bilan.nikita@nppntt.ru>
2026-03-18 16:33:01 +0300

    code refactoring

--- words/words/words/words.go
+++ words/words/words/words.go
@@ -1,32 +1,14 @@
-package main
+package words
 
 import (
-	"context"
-	"errors"
-	"flag"
-	"log"
 	"log/slog"
-	"net"
-	"os/signal"
 	"regexp"
 	"strings"
-	"syscall"
 
 	"github.com/kljensen/snowball"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/emptypb"
-	wordspb "yadro.com/course/proto/words"
 )
 
-const maxPhraseSize = 4 * 1024
 const averageRequestWords = 4
-
-type server struct {
-	wordspb.UnimplementedWordsServer
-}
 
 var wordRegexp = regexp.MustCompile(`[[:alnum:]]+`)
 
@@ -78,22 +60,7 @@ var stopWords = map[string]struct{}{
 	"you": {}, "your": {}, "yours": {}, "yourself": {}, "yourselves": {},
 }
 
-func (s *server) Ping(_ context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Norm(ctx context.Context, in *wordspb.WordsRequest) (*wordspb.WordsReply, error) {
-
-	if len(in.Phrase) > maxPhraseSize {
-		return nil, status.Error(codes.ResourceExhausted, "phrase too large")
-	}
-
-	words := normalize(ctx, in.Phrase)
-
-	return &wordspb.WordsReply{Words: words}, nil
-}
-
-func normalize(ctx context.Context, phrase string) []string {
+func Norm(phrase string) []string {
 
 	words := wordRegexp.FindAllString(phrase, -1)
 
@@ -101,12 +68,6 @@ func normalize(ctx context.Context, phrase string) []string {
 	seen := make(map[string]struct{}, averageRequestWords)
 
 	for _, word := range words {
-
-		select {
-		case <-ctx.Done():
-			return result
-		default:
-		}
 
 		word = strings.ToLower(word)
 
@@ -129,48 +90,4 @@ func normalize(ctx context.Context, phrase string) []string {
 	}
 
 	return result
-}
-
-func main() {
-	configPath := flag.String("config", "config.yaml", "config path")
-	addrFlag := flag.String("address", "", "server address")
-
-	flag.Parse()
-
-	address, port, err := ParseServerConfig(*configPath, *addrFlag)
-
-	if err != nil {
-		log.Fatalf("Error parsing server config: %v", err)
-	}
-	slog.Info("server config",
-		"address", address,
-		"port", port,
-	)
-
-	listener, err := net.Listen("tcp", address)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	s := grpc.NewServer()
-	wordspb.RegisterWordsServer(s, &server{})
-	reflection.Register(s)
-
-	go func() {
-		if err := s.Serve(listener); err != nil && !errors.Is(err, grpc.ErrServerStopped) {
-			log.Fatalf("failed to serve: %v", err)
-		}
-	}()
-
-	ctx, stop := signal.NotifyContext(context.Background(),
-		syscall.SIGINT,
-		syscall.SIGTERM,
-	)
-	defer stop()
-
-	<-ctx.Done()
-
-	slog.Info("shutting down")
-
-	s.GracefulStop()
 }
